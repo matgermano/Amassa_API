bf5da009b925460a439f6aa18e69bf5a28cbe7fa
Rafael <rafaelsfsoares@gmail.com>
2021-11-24 16:08:22 -0300

    FIX: require express

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+const entradas = require('./entradasRoutes');
+const express = require('express');
+
+module.exports = app => {
+    app.use((req, res, next) => {
+        console.log(req.headers.host, new Date().toLocaleTimeString());
+        next();
+    });
+
+    app.use(express.json());
+    app.use(entradas)
+    
+}
