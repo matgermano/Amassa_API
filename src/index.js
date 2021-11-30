97c5b110ab58d3be08e125383b253f3f49eb7534
Rafael <rafaelsfsoares@gmail.com>
2021-11-30 13:16:32 -0300

    FEAT:HOST

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const routes = require('./routes');
 const app = express();
 require('dotenv').config()
-const port = process.env.PORT || 3000;
+const port = process.env.HOST|| 3000;
 
 
 routes(app)
