99c02437f1f9d5d0828a38ee0c9c3aa88a0ea54c
Rafael <rafaelsfsoares@gmail.com>
2021-11-30 12:48:02 -0300

    FEAT: dotenv installed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require ('express');
 const routes = require('./routes');
 const app = express();
-const port = 3000;
+require('dotenv').config()
+const port = process.env.PORT || 3000;
 
 
 routes(app)
