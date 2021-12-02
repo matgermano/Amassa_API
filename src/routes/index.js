da2827d7eeed46e2d8a367d2c2ebedf18c25fdc7
Rafael <rafaelsfsoares@gmail.com>
2021-12-01 22:55:52 -0300

    FEAT: Adicionando rotas para nova tabela(pizzas)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,15 @@
-const bodyParser=require('body-parser')
-const pizza=require('./pizzasRoute')
-module.exports=app=>{
-    app.use(bodyParser.json())
+const pizza = require('./pizzasRoute')
+const entradas = require('./entradasRoutes');
+const express = require('express');
+
+module.exports = app => {
+    app.use((req, res, next) => {
+        console.log(req.headers.host, new Date().toLocaleTimeString());
+        next();
+    });
+
+    app.use(express.json());
+    app.use(entradas)
     app.use(pizza)
-    
+
 }
