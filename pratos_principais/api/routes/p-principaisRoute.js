2a214c965e49dfbec370014e92beffc69b3ff5d4
Camila Rangel <camilafariasrangel@gmail.com>
2021-12-03 14:45:23 -0300

    REFACT: ajustando routes e seeders

--- pratos_principais/api/routes/p-principaisRoute.js
+++ pratos_principais/api/routes/p-principaisRoute.js
@@ -3,6 +3,10 @@ const Pratos_principaisController = require('../controllers/Pratos_principaisCon
 
 const router = Router();
 
-router.get('/pratos-principais', Pratos_principaisController.pegaPprincipais)
+router.get('/pratos-principais', Pratos_principaisController.pegaPprincipais);
+router.get('/pratos-principais/:id', Pratos_principaisController.show);
+router.post('/pratos-principais', Pratos_principaisController.save);
+router.put('/pratos-principais/:id', Pratos_principaisController.update);
+router.delete('/pratos-principais/:id', Pratos_principaisController.remove);
 
 module.exports = router;
--- pratos_principais/api/seeders/20211130093858-demo-pratos-principais.js
+++ pratos_principais/api/seeders/20211130093858-demo-pratos-principais.js
@@ -1,19 +1,42 @@
-'use strict';
-
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-   await queryInterface.bulkInsert('pratos_principais', [{
-        nome: "nhoque da mama",
-        ingredientes: "Nhoque com molho branco ou vermelho.",
-        porcao: "Para dividir",
-        preco: "R$55,00",
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }], {});
+  up: (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert('pratos_principais', [
+            {
+                nome: 'lasanha bolonhesa',
+                ingredientes: "Ragu de carne, molho de tomate, parmesão e muçarela gratinada. ",
+                porcao: 'Para dividir',
+                preco: 'R$60,00',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                nome: 'risoto tropical',
+                ingredientes: "Salmão grelhado com risoto de limão siciliano.",
+                porcao: 'Individual',
+                preco: 'R$45,00',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                nome: 'nhoque da mama',
+                ingredientes: "Nhoque com molho branco ou vermelho. ",
+                porcao: 'Para dividir',
+                preco: 'R$55,00',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                nome: 'carbonara do cristo',
+                ingredientes: "Calabresa, parmesão, gema de ovo e pimenta do reino.",
+                porcao: 'Individual',
+                preco: 'R$30,00',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
+    ], {})
   },
 
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('People', null, {});
-  
+  down: (queryInterface, Sequelize) => {
+        return queryInterface.bulkDelete('pratos_principais', null, {})
   }
-};
+}
