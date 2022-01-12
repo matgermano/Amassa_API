b4a3f73476897653106cca1a9478c6844b11d2c4
Matheus <germano.prog@gmail.com>
2022-01-11 23:42:25 -0300

    Fix: Listen Port Heroku

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ const routes=require("./routes/index")
 const app = express()
 const port = 3000
 routes(app)
-app.listen(port, () => { console.log(`Servidor rodando na porta ${port}`); })
+app.listen( process.env.PORT || 3000, () => { console.log(`Servidor rodando na porta ${port}`); })
 module.exports=app
