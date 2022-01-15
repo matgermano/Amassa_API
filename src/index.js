71122c8eceee7fc267ce32ade6c88026e2b8aff7
Matheus <germano.prog@gmail.com>
2022-01-15 17:36:53 -0300

    Feat: Add Cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const routes=require("./routes/index")
 const app = express()
 const port = 3000
+const cors = require('cors')
+app.use(cors())
 routes(app)
 app.listen( process.env.PORT || 3000, () => { console.log(`Servidor rodando na porta ${port}`); })
 module.exports=app
